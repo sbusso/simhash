4acc331671cbdcc2ec1d168cff82729f0ed10a7e
Stéphane Busso <sbusso@users.noreply.github.com>
2015-08-10 21:08:06 +0700

    Update stopwords.rb

--- lib/simhash/stopwords.rb
+++ lib/simhash/stopwords.rb
@@ -1,8 +1,9 @@
 require File.join(File.dirname(__FILE__), "stopwords", "en")
 require File.join(File.dirname(__FILE__), "stopwords", "ru")
+require File.join(File.dirname(__FILE__), "stopwords", "fr")
 
 module Simhash
   module Stopwords
-    ALL = RU + EN
+    ALL = FR + EN
   end
 end
